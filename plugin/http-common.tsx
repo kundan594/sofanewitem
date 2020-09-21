8adc55330fdd6aad44ec8fba0638fc548493c8ee
kundan <kundankumar594@gmail.com>
2020-09-21 07:04:30 -0700

    added

--- plugin/http-common.tsx
+++ plugin/http-common.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "https://95.168.161.223/testui/",
-  baseURL: "https://cms-int.so.fa.dog/testui/",
+  baseURL: "https://cms-int.so.fa.dog/",
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
