603cebbf5bfa90f754a75c4eeff4cf6a9f1fa3bd
kundan <kundankumar594@gmail.com>
2020-10-05 21:04:05 -0700

    added

--- components/cardDisplay/DisplayInfo.tsx
+++ components/cardDisplay/DisplayInfo.tsx
@@ -66,8 +66,7 @@ const DisplayInfo = () => {
   function uplaodVideo(item, apiEndPoint,source, name, size, file){
     let data = FetchDataService.uplaodVideo(item, apiEndPoint,source, name, size, file)
       .then((response) => {
-        fetchData1();
-      
+        fetchData1();      
       })
       .catch((e) => {
         console.log(e);
@@ -324,10 +323,35 @@ const DisplayInfo = () => {
     </>
   ));
   return (
+    <div>
+    <div className="col-md-6">        
+    <div className="mt-2 md:flex md:items-center md:justify-between">
+      <div className="flex-2 min-w-0">
+        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
+          Fetch Data
+        </h2>
+      </div>
+      <div className="mt-4 flex-shrink-0 flex md:mt-0 md:ml-4">
+        <span className="ml-3 shadow-sm rounded-md">
+        <Link href="additem">
+          <button
+            type="button"
+            className="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out"
+          >
+           Add item
+          </button>
+          </Link>
+        </span>
+      </div>
+    </div>
     <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {fetchDataInfo}
     </ul>
 
+    </div>
+ 
+</div>
+    
   
     
   );
