bb69d0ad1b292dc485ff465cfc8ce21a091d0e9c
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-10-06 12:11:46 +0530

    UI fix

--- components/cardDisplay/DisplayInfo.tsx
+++ components/cardDisplay/DisplayInfo.tsx
@@ -4,284 +4,301 @@ import FetchDataService from "../../pages/api/fetch.service";
 import Link from "next/link";
 import Router from "next/router";
 
-import Popover  from '../formComponent/PopoverRender';
+import Popover from '../formComponent/PopoverRender';
 import { PreviewDataService } from "../../pages/api/previewService";
 const DisplayInfo = () => {
-  const [DataAll, setfetchData] = useState(null);
-  useEffect(() => {
-    fetchData1();
-  }, []);
-  function fetchData1() {
-    let data = FetchDataService.getAll1()
-   // setfetchData(data);
-    .then(response => {
-        setfetchData(response.data);
-        // console.log(response.data,"response ");
-        // console.log(response.data,"response.data.data");
-    })
-    .catch(e => {
-        console.log(e);
-    });
-  }
+	const [DataAll, setfetchData] = useState(null);
 
-  function fetchData() {
-    let data = FetchDataService.getAll();
-     setfetchData(data);
-    
-  }
+	useEffect(() => {
+		let isSubscribed = true;
+		FetchDataService.getAll1()
+			// setfetchData(data);
+			.then(response => {
+				if (isSubscribed) {
+					setfetchData(response.data);
+				}
+			})
+			.catch(e => {
+				console.log(e);
+			});
 
-  function actionPerformed(item, apiEndPoint,e) {
-    e.preventDefault();
-    if(apiEndPoint =="Preview Clips"){
-      console.log(item,"item====");
-      ///PreviewDataService.clearData();
-      PreviewDataService.setData(item);
-      Router.push(
-        '/preview'
-        );
-      
-      
-    }
-    //processedDataInfo(item, apiEndPoint,e);
-  }
+		return () => (isSubscribed = false)
+	});
 
-  function processedDataInfo(item, apiEndPoint,e) {
-    e.preventDefault();
-    console.log(item, "asdas");
-    let data = FetchDataService.processedData(item, apiEndPoint)
-      .then((response) => {
-        fetchData1();
-      
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }
+	function fetchData1() {
+		let data = FetchDataService.getAll1()
+			// setfetchData(data);
+			.then(response => {
+				setfetchData(response.data);
+				// console.log(response.data,"response ");
+				// console.log(response.data,"response.data.data");
+			})
+			.catch(e => {
+				console.log(e);
+			});
+	}
 
-  function actionRender(item) {
-    switch (item.state) {
-      case "new": {
-        return (
-          <a
-            onClick={(e) => actionPerformed(item, "submit",e)}
-            href="javascript:void(0);"
-          >
-            Submit
-          </a>
-        );
-      }
-      case "awaiting_review_by_lead_journalist": {
-        // return  'Approve | Reject'
-        return (
-          <div>
-            <a
-              onClick={(e) => actionPerformed(item, "lead_journalist_approve",e)}
-              href="javascript:void(0);"
-            >
-              Approve
+	function fetchData() {
+		let data = FetchDataService.getAll();
+		setfetchData(data);
+
+	}
+
+	function actionPerformed(item, apiEndPoint, e) {
+		e.preventDefault();
+		if (apiEndPoint == "Preview Clips") {
+			console.log(item, "item====");
+			///PreviewDataService.clearData();
+			PreviewDataService.setData(item);
+			Router.push(
+				'/preview'
+			);
+
+
+		}
+		//processedDataInfo(item, apiEndPoint,e);
+	}
+
+	function processedDataInfo(item, apiEndPoint, e) {
+		e.preventDefault();
+		console.log(item, "asdas");
+		let data = FetchDataService.processedData(item, apiEndPoint)
+			.then((response) => {
+				fetchData1();
+
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	}
+
+	function actionRender(item) {
+		switch (item.state) {
+			case "new": {
+				return (
+					<a
+						onClick={(e) => actionPerformed(item, "submit", e)}
+						href="javascript:void(0);"
+					>
+						Submit
+					</a>
+				);
+			}
+			case "awaiting_review_by_lead_journalist": {
+				// return  'Approve | Reject'
+				return (
+					<div>
+						<a
+							onClick={(e) => actionPerformed(item, "lead_journalist_approve", e)}
+							href="javascript:void(0);"
+						>
+							Approve
             </a>{" "}
             |{" "}
-            <a
-              href="javascript:void(0);"
-              onClick={(e) => actionPerformed(item, "lead_journalist_reject",e)}
-            >
-              Reject
+						<a
+							href="javascript:void(0);"
+							onClick={(e) => actionPerformed(item, "lead_journalist_reject", e)}
+						>
+							Reject
             </a>{" "}
-          </div>
-        );
-      }
-      case "awaiting_video_upload": {
-        return "Upload video";
-      }
-      case "awaiting_review_by_lead_video_editor": {
-        return (
-          <div>
-            <a
-              onClick={(e) => actionPerformed(item, "Preview Clips",e)}
-              href="javascript:void(0);"
-            >
-              Preview Clips
+					</div>
+				);
+			}
+			case "awaiting_video_upload": {
+				return "Upload video";
+			}
+			case "awaiting_review_by_lead_video_editor": {
+				return (
+					<div>
+						<a
+							onClick={(e) => actionPerformed(item, "Preview Clips", e)}
+							href="javascript:void(0);"
+						>
+							Preview Clips
             </a>{" "}
             |{" "}
-            <a
-              onClick={(e) =>
-                actionPerformed(item, "lead_video_editor_approve",e)
-              }
-              href="javascript:void(0);"
-            >
-              Approve
+						<a
+							onClick={(e) =>
+								actionPerformed(item, "lead_video_editor_approve", e)
+							}
+							href="javascript:void(0);"
+						>
+							Approve
             </a>{" "}
-            <a
-              href="javascript:void(0);"
-              onClick={(e) => actionPerformed(item, "lead_video_editor_reject",e)}
-            >
-              Reject
+						<a
+							href="javascript:void(0);"
+							onClick={(e) => actionPerformed(item, "lead_video_editor_reject", e)}
+						>
+							Reject
             </a>{" "}
-          </div>
-        );
-      }
-      case "ready_for_push": {
-        return (
-          <a
-            onClick={(e) => actionPerformed(item, "push_to_feed",e)}
-            href="javascript:void(0);"
-          >
-            Push To Feed
-          </a>
-        );
-      }
-      case "pushed_to_feed": {
-        return (
-          <a
-            onClick={(e) => actionPerformed(item, "remove_from_feed",e)}
-            href="javascript:void(0);"
-          >
-            Remove from Feed
-          </a>
-        );
-      }
-      case "removed_from_feed": {
-        return (
-          <a
-            onClick={(e) => actionPerformed(item, "push_to_feed",e)}
-            href="javascript:void(0);"
-          >
-            Push To Feed
-          </a>
-        );
-      }
-      default: {
-        return "no  state matched --- think that ";
-      }
-    }
-  }
+					</div>
+				);
+			}
+			case "ready_for_push": {
+				return (
+					<a
+						onClick={(e) => actionPerformed(item, "push_to_feed", e)}
+						href="javascript:void(0);"
+					>
+						Push To Feed
+					</a>
+				);
+			}
+			case "pushed_to_feed": {
+				return (
+					<a
+						onClick={(e) => actionPerformed(item, "remove_from_feed", e)}
+						href="javascript:void(0);"
+					>
+						Remove from Feed
+					</a>
+				);
+			}
+			case "removed_from_feed": {
+				return (
+					<a
+						onClick={(e) => actionPerformed(item, "push_to_feed", e)}
+						href="javascript:void(0);"
+					>
+						Push To Feed
+					</a>
+				);
+			}
+			default: {
+				return "no  state matched --- think that ";
+			}
+		}
+	}
 
-  const fetchDataInfo = DataAll?.news_items?.map((item, i) => (
-    <>
-    
-      <li className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow">
-        <div className="flex-1 truncate p-2 space-y-2 text-sm">
-          <p className="mt-1 text-gray-500 text-sm leading-5 truncate">New </p>
-          <div className="flex content-between flex-wrap ">
-            <div className="w-2/5 text-left ">
-              <div className="text-gray-500">Title</div>
-            </div>
-            <div className="w-3/5  text-left ">
-              <div className="text-gray-700  ">{item.title}</div>
-            </div>
-          </div>
-          <div className="flex content-between flex-wrap ">
-            <div className="w-2/5 text-left ">
-              <div className="text-gray-500  ">Category</div>
-            </div>
-            <div className="w-3/5  text-left ">
-              <div className="text-gray-700  ">{item.category}</div>
-            </div>
-          </div>
-          <div className="flex content-between flex-wrap">
-            <div className="w-2/5 text-left ">
-              <div className="text-gray-500  ">News Credits</div>
-            </div>
-            <div className="w-3/5  text-left ">
-            <div className="text-gray-700  ">
-                 {
-                 item.news_credits?.map((news_credit, index) => (     
-                <>       
-                 <a href="{news_credit.url}">{news_credit.link_text}</a>  
-                 <span> | </span>
-                 </>
-                ))
-                }
+	const fetchDataInfo = DataAll?.news_items?.map((item, i) => (
+		<>
 
-            </div>
+			<li className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow">
+				<div className="flex-1 truncate p-2 space-y-2 text-sm">
+					<p className="mt-1 text-gray-500 text-sm leading-5 truncate">New </p>
+					<div className="flex content-between flex-wrap ">
+						<div className="w-2/5 text-left ">
+							<div className="text-gray-500">Title</div>
+						</div>
+						<div className="w-3/5  text-left ">
+							<div className="text-gray-700  ">{item.title}</div>
+						</div>
+					</div>
+					<div className="flex content-between flex-wrap ">
+						<div className="w-2/5 text-left ">
+							<div className="text-gray-500  ">Category</div>
+						</div>
+						<div className="w-3/5  text-left ">
+							<div className="text-gray-700  ">{item.category}</div>
+						</div>
+					</div>
+					<div className="flex content-between flex-wrap">
+						<div className="w-2/5 text-left ">
+							<div className="text-gray-500  ">News Credits</div>
+						</div>
+						<div className="w-3/5  text-left ">
+							<div className="text-gray-700  ">
+								{
+									item.news_credits?.map((news_credit, index) => (
+										<>
+											<a href="{news_credit.url}">{news_credit.link_text}</a>
+											<span> | </span>
+										</>
+									))
+								}
 
-            </div>
-          </div>
-          <div className="flex content-between flex-wrap">
-            <div className="w-2/5 text-left ">
-              <div className="text-gray-500  ">Visual Credits</div>
-            </div>
-            <div className="w-3/5  text-left ">
-              <div className="text-gray-700  ">
-              {
-                 item.visual_credits?.map((visual_credit, index) => (     
-                <>       
-                 <a href="{visual_credit.url}">{visual_credit.link_text}</a>  
-                 <span> | </span>
-                 </>
-                ))
-                }
+							</div>
 
-              </div>
-            </div>
-          </div>
-          <div className="flex content-between flex-wrap">
-            <div className="w-2/5 text-left ">
-              <div className="text-gray-700  ">Tags</div>
-            </div>
-            <div className="w-3/5  text-left ">
-              <div className="text-gray-700  ">{item.tags}</div>
-            </div>
-          </div>
+						</div>
+					</div>
+					<div className="flex content-between flex-wrap">
+						<div className="w-2/5 text-left ">
+							<div className="text-gray-500  ">Visual Credits</div>
+						</div>
+						<div className="w-3/5  text-left ">
+							<div className="text-gray-700  ">
+								{
+									item.visual_credits?.map((visual_credit, index) => (
+										<>
+											<a href="{visual_credit.url}">{visual_credit.link_text}</a>
+											<span> | </span>
+										</>
+									))
+								}
 
-          <div className="flex content-between flex-wrap">
-            <div className="w-2/5 text-left ">
-              <div className="text-gray-700  ">State</div>
-            </div>
-            <div className="w-3/5  text-left ">
-              <div className="text-gray-700  ">{item.state}</div>
-            </div>
-          </div>
+							</div>
+						</div>
+					</div>
+					<div className="flex content-between flex-wrap">
+						<div className="w-2/5 text-left ">
+							<div className="text-gray-700  ">Tags</div>
+						</div>
+						<div className="w-3/5  text-left ">
+							<div className="text-gray-700  ">{item.tags}</div>
+						</div>
+					</div>
 
-          <div className="flex content-between flex-wrap">
-             
-             
-            <div className="w-2/5 text-left ">
-              <div className="text-gray-700  ">Description</div>
-            </div>
-            <div className="w-3/5  text-left ">
-              <div className="text-gray-700  ">
-              { item.descriptions.map(descitem => (
+					<div className="flex content-between flex-wrap">
+						<div className="w-2/5 text-left ">
+							<div className="text-gray-700  ">State</div>
+						</div>
+						<div className="w-3/5  text-left ">
+							<div className="text-gray-700  ">{item.state}</div>
+						</div>
+					</div>
 
-                  descitem.sentences.map(sentence => (
-                  <span>  {sentence} </span>       
-                    ))
+					<div className="flex content-between flex-wrap">
 
-                  ))}
-                
-                </div>
-            </div>
-          </div>
-        </div>
-        <div className="border-t border-gray-200">
-          <div className="-mt-px flex">
-            <div className="w-0 flex-1 flex border-r border-gray-200">
-              <a
-                 href="javascript:void(0);"
-                className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150"
-              >                
-                <span className="ml-3">{actionRender(item)}</span>
-              </a>
-            </div>
-            <div className="-ml-px w-0 flex-1 flex">
-              <a
-                href="javascript:void(0);"
-                className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150"
-              >                
-                {/* <span className="ml-3">ReadMore</span> */}
-                <Popover color="pink" dataInfo = {item} />
-              </a>
-            </div>
-          </div>
-        </div>
-      </li>
-    </>
-  ));
-  return (
-    <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {fetchDataInfo}
-    </ul>
-  );
+
+						<div className="w-2/5 text-left ">
+							<div className="text-gray-700  ">Description</div>
+						</div>
+						<div className="w-3/5  text-left ">
+							<div className="text-gray-700  ">
+								{item.descriptions.map(descitem => (
+
+									descitem.sentences.map(sentence => (
+										<span>  {sentence} </span>
+									))
+
+								))}
+
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="border-t border-gray-200">
+					<div className="-mt-px flex">
+						<div className="w-0 flex-1 flex border-r border-gray-200">
+							<a
+								href="javascript:void(0);"
+								className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150"
+							>
+								<span className="ml-3">{actionRender(item)}</span>
+							</a>
+						</div>
+						<div className="-ml-px w-0 flex-1 flex">
+							<a
+								href="javascript:void(0);"
+								className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm leading-5 text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 transition ease-in-out duration-150"
+							>
+								{/* <span className="ml-3">ReadMore</span> */}
+								<Popover color="pink" dataInfo={item} />
+							</a>
+						</div>
+					</div>
+				</div>
+			</li>
+		</>
+	));
+	return (
+		<>
+			<button onClick={()=>fetchData1()} className="px-2 py-2 bg-indigo-600 text-white text-sm rounded my-2">Fetch Data</button>
+			<ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+				{fetchDataInfo}
+			</ul>
+		</>
+	);
 };
 
 export default DisplayInfo;
--- pages/preview.js
+++ pages/preview.js
@@ -5,108 +5,104 @@ import Link from "next/link";
 import Router from "next/router";
 
 const PreviewClip = () => {
-  const [clipAll, setclipData] = useState(null);
-   let datainfo = '';
-   let data ='';
-   let clipDataInfo= null;
-  useEffect(() => {
-   /// PreviewDataService.clearData();
-   PreviewDataService.getData().subscribe(message => {
+	const [clipAll, setClipAll] = useState(null);
+	let datainfo = null;
+	let data = '';
+	let clipDataInfo = null;
+	useEffect(() => {
+		/// PreviewDataService.clearData();
+		let isSubscribed = true;
+		//console.log("isSubscribed: ",isSubscribed);
+		//console.log("datainfo: ",datainfo);
+		if (datainfo == null) {
 
-    datainfo  = message.value;
-    console.log(datainfo,"datainfo");
-    console.log(message.value);
-    test();
+			let d = PreviewDataService.getData().subscribe(message => {
+				console.log("message: ",message);
+				if (isSubscribed) {
+					console.log("in if isSubscribed: ",isSubscribed);
+					datainfo = message.value;
+					setClipAll(datainfo);
+					console.log(datainfo, "datainfo");
+					console.log("set clipAll", clipAll);
+					isSubscribed = false;
+				}
 
- });
-   
-  });
+				//console.log(message.value);
+			});
+			//console.log("PreviewDataService: ",d);
+		}
+		return () => (isSubscribed)
+	});
 
-  useEffect(() => {
-      if(datainfo !=""){
-       // setclipData(datainfo);      
-        clipDataInfo  =  datainfo?.clips?.map((item, i) => (
-            <>
-            <li>
-                 <div className="space-y-4">
-                   <div className="relative pb-2/3">
-                     <img className="absolute object-cover h-full w-full shadow-lg rounded-lg" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="" />
-                   </div>
-                 </div>
-               </li>
-            </>
-           ));
-    
-       console.log(clipDataInfo,"clipDataInfo");
-      }
-   
+	useEffect(() => {
+		let isSubscribed = true;
+		console.log("bfr if clipAll", clipAll);
+		if (clipAll != null && isSubscribed) {
+			console.log("in if clipAll", clipAll);
+
+			clipAll?.clips?.map((item, i) => {
+				console.log("item: ", item);
+			});
+		}
+		console.log("after if clipAll", clipAll);
+		return () => (isSubscribed = false)
+	}, [clipAll]);
+
+	console.log(datainfo, "datainfo end");
+
+	return (
+
+		<div className="bg-white">
+			<div className="mx-auto py-12 px-4 max-w-screen-xl sm:px-6 lg:px-8 lg:py-24">
+				<div className="space-y-12">
+					<div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+						<h2 className="text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl">Our Video clips </h2>
+						{/* <p className="text-xl leading-7 text-gray-500">Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor ultricies donec risus sodales. Tempus quis et.</p> */}
+					</div>
+					<ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
+						{/* <li>
+							<div className="space-y-4">
+								<div className="relative pb-2/3">
+								<img className="absolute object-cover h-full w-full shadow-lg rounded-lg" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="" />
+								</div>
+							</div>
+							</li>
+
+							<li>
+							<div className="space-y-4">
+								<div className="relative pb-2/3">
+								<img className="absolute object-cover h-full w-full shadow-lg rounded-lg" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="" />
+								</div>
+							</div>
+							</li> */}
+
+						{datainfo != null && (
+							datainfo.clips.map((item, i) => {
+								<li>
+									<div className="space-y-4">
+										<div className="relative pb-2/3">lllllllllllllllll
+                         						<img className="absolute object-cover h-full w-full shadow-lg rounded-lg" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="" ></img>
+										</div>
+									</div>
+								</li>
+							})
+						)}
+
+						<li>
+							<div className="space-y-4">
+								<div className="relative pb-2/3">
+									<img className="absolute object-cover h-full w-full shadow-lg rounded-lg" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="" />
+								</div>
+							</div>
+						</li>
 
 
-  },[datainfo]);
+					</ul>
+				</div>
+			</div>
+		</div>
 
-  function test(){
-    console.log(clipAll,"clipAll");
-  }
-
-
-
-
-
- 
-
- 
-
-
-
-  
-
-  return (
-
-<div className="bg-white">
-  <div className="mx-auto py-12 px-4 max-w-screen-xl sm:px-6 lg:px-8 lg:py-24">
-    <div className="space-y-12">
-      <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
-        <h2 className="text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl">Our Video clips </h2>
-        {/* <p className="text-xl leading-7 text-gray-500">Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor ultricies donec risus sodales. Tempus quis et.</p> */}
-      </div>
-      <ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
-        {/* <li>
-          <div className="space-y-4">
-            <div className="relative pb-2/3">
-              <img className="absolute object-cover h-full w-full shadow-lg rounded-lg" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="" />
-            </div>
-          </div>
-        </li>
-
-        <li>
-          <div className="space-y-4">
-            <div className="relative pb-2/3">
-              <img className="absolute object-cover h-full w-full shadow-lg rounded-lg" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="" />
-            </div>
-          </div>
-        </li> */}
-
-         {datainfo != "" && (
-              datainfo.clips.map((item, i) => (
-                <>
-                <li>
-                     <div className="space-y-4">
-                       <div className="relative pb-2/3">lllllllllllllllll
-                         <img className="absolute object-cover h-full w-full shadow-lg rounded-lg" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="" />
-                       </div>
-                     </div>
-                   </li>
-                </>
-               ))
-         )}
-
-       
-      </ul>
-    </div>
-  </div>
-</div>
-
-  );
+	);
 };
 
 export default PreviewClip;
