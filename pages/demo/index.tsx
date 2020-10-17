ea39c9160898e3cfa630061fb458267787eab1f5
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-10-17 18:28:38 +0530

    Buttons added -Add sentence -Add credit

--- pages/demo/index.tsx
+++ pages/demo/index.tsx
@@ -4,8 +4,14 @@ import Layout from '../../components/layout/Layout';
 
 const Demo = () => {
 
+    const [openTagDropdown, setOpenTagDropdown] = useState(false);
+    const toggleTagDropdown = () => { setOpenTagDropdown(!openTagDropdown) };
+
+    const [openCategoryDropdown, setOpenCategoryDropdown] = useState(false);
+    const toggleCateDropdown = () => { setOpenCategoryDropdown(!openCategoryDropdown) };
+
     return (
-        <div className="w-full h-full bg-gray-400">
+        <div className="w-full h-full bg-black">
             <nav className="bg-gray-800">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
@@ -338,18 +344,24 @@ const Demo = () => {
                         <div className="w-11/12 mx-auto flex-none float-left">
                             <div className="md:flex shadow-lg mx-6 md:mx-auto w-full h-xl">
                                 <div className="w-full md:w-1/5 relative z-10">
-                                    <div className="absolute inset-x-0 top-0 transform translate-y-px">
-                                        <div className="flex justify-center transform translate-y-1/2">
-                                            <span className="bg-red-400 inline-flex w-full h-10 flex items-center justify-center text-center px-4 py-1 text-xs leading-5 font-semibold tracking-wider uppercase text-white shadow-lg">
-                                                Pushed To Feed
-                                            </span>
+                                    <div className="h-full w-full bg-gray-100 border-r border-gray-200 p-2 rounded-lg rounded-r-none flex items-start">
+                                        <div className="w-full flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                                            <div className="text-center">
+                                                <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48">
+                                                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                                </svg>
+                                                <p className="mt-1 text-sm text-gray-600">
+                                                    <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition duration-150 ease-in-out">
+                                                        Upload a file
+                                                    </button>
+                                                    or drag and drop
+                                                </p>
+                                                <p className="mt-1 text-xs text-gray-500">
+                                                    MP4, MOV, WMV up to 500MB
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className="h-full w-full bg-red-400 rounded-lg rounded-r-none flex items-start">
-                                        <img className="h-auto w-full shadow-2xl rounded-lg rounded-r-none rounded-b-none"
-                                            src="https://cdn.so.fa.dog/thumbnails-int/76c3923c-0f5f-11eb-8a82-01bf9fcdf51b.jpg_43.jpg" alt="bag" />
-                                    </div>
-
                                 </div>
                                 <div className="relative w-full h-full md:w-4/5 px-4 py-2 bg-white rounded-r-lg border-r-8 border-red-400">
                                     <div className="py-2">
@@ -380,25 +392,26 @@ const Demo = () => {
                                                         </a>
                                                     </nav>
                                                 </div>
-                                                <div className="mt-4" role="group" aria-labelledby="teams-headline">
-                                                    <div className="group flex items-center px-3 text-xs leading-5 font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition ease-in-out duration-150">
+                                                <div className="mt-4 space-y-1" role="group" aria-labelledby="teams-headline">
+                                                    {/* <div className="group flex items-center px-3 text-xs leading-5 font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition ease-in-out duration-150">
                                                         <span className="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full"></span>
-                                                        <span className="truncate">
-                                                            Engineering
-                                                        </span>
-                                                    </div>
-                                                    <div className="group flex items-center px-3 text-xs leading-5 font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition ease-in-out duration-150">
+                                                        <div className="w-full mt-1 relative rounded-md shadow-sm">
+                                                            <input id="text" className="form-input block w-full text-xs sm:leading-3" placeholder="Enter sentence" />
+                                                        </div>
+                                                    </div> */}
+                                                    <div className="group px-3 flex items-center leading-5 font-medium">
                                                         <span className="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full"></span>
-                                                        <span className="truncate">
-                                                            Engineering
-                                                        </span>
+                                                        <button className="text-white px-2 py-1 bg-indigo-600 rounded text-xs">+ Add Sentence</button>
                                                     </div>
-                                                    <div className="group flex items-center px-3 text-xs leading-5 font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition ease-in-out duration-150">
+                                                    <div className="group px-3 flex items-center leading-5 font-medium">
                                                         <span className="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full"></span>
-                                                        <span className="truncate">
-                                                            Engineering
-                                                        </span>
+                                                        <button className="text-white px-2 py-1 bg-indigo-600 rounded text-xs">+ Add Sentence</button>
                                                     </div>
+                                                    <div className="group px-3 flex items-center leading-5 font-medium">
+                                                        <span className="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full"></span>
+                                                        <button className="text-white px-2 py-1 bg-indigo-600 rounded text-xs">+ Add Sentence</button>
+                                                    </div>
+
                                                 </div>
                                             </div>
                                         </div>
@@ -416,24 +429,16 @@ const Demo = () => {
                                                         </a>
                                                     </nav>
                                                 </div>
-                                                <div className="mt-4" role="group" aria-labelledby="teams-headline">
+                                                <div className="mt-4 space-y-1" role="group" aria-labelledby="teams-headline">
                                                     <div className="group flex items-center px-3 text-xs leading-5 font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition ease-in-out duration-150">
                                                         <span className="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full"></span>
                                                         <span className="truncate">
                                                             Engineering
                                                         </span>
                                                     </div>
-                                                    <div className="group flex items-center px-3 text-xs leading-5 font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition ease-in-out duration-150">
+                                                    <div className="group px-3 flex items-center leading-5 font-medium">
                                                         <span className="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full"></span>
-                                                        <span className="truncate">
-                                                            Engineering
-                                                        </span>
-                                                    </div>
-                                                    <div className="group flex items-center px-3 text-xs leading-5 font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition ease-in-out duration-150">
-                                                        <span className="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full"></span>
-                                                        <span className="truncate">
-                                                            Engineering
-                                                        </span>
+                                                        <button className="text-white px-2 py-1 bg-indigo-600 rounded text-xs">+ Add News Credit</button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -441,14 +446,58 @@ const Demo = () => {
                                     </div>
                                     <div className="absolute mb-4 mr-4 bottom-0 inset-x-0">
                                         <div className="w-full space-x-2 flex justify-end">
+                                            <div className="relative inline-block text-left">
+                                                <div>
+                                                    <span onClick={toggleTagDropdown} className="rounded-md shadow-sm">
+                                                        <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 px-2 py-0.5 bg-white text-xs leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150" id="options-menu" aria-haspopup="true" aria-expanded="true">
+                                                            Tags
+                                                        <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                            </svg>
+                                                        </button>
+                                                    </span>
+                                                </div>
+                                                {openTagDropdown && (
+                                                    <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg">
+                                                        <div className="rounded-md bg-white shadow-xs">
+                                                            <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                                                                <a href="#" className="block px-4 py-1 text-xs leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">
+                                                                    Local Estonian Content
+                                                                </a>
+                                                                <a href="#" className="block px-4 py-1 text-xs leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">
+                                                                    NSFW
+                                                                </a>
+                                                                <a href="#" className="block px-4 py-1 text-xs leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">
+                                                                    NSFL
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                )}
+                                            </div>
                                             <span className="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium leading-4 bg-blue-100 text-blue-800">
                                                 Local Estonian Content
+                                                <button type="button" className="flex-shrink-0 ml-1.5 inline-flex text-indigo-500 focus:outline-none focus:text-indigo-700" aria-label="Remove small badge">
+                                                    <svg className="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
+                                                        <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
+                                                    </svg>
+                                                </button>
                                             </span>
                                             <span className="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium leading-4 bg-blue-100 text-blue-800">
                                                 NSFW
+                                                <button type="button" className="flex-shrink-0 ml-1.5 inline-flex text-indigo-500 focus:outline-none focus:text-indigo-700" aria-label="Remove small badge">
+                                                    <svg className="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
+                                                        <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
+                                                    </svg>
+                                                </button>
                                             </span>
                                             <span className="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium leading-4 bg-blue-100 text-blue-800">
                                                 NSFL
+                                                <button type="button" className="flex-shrink-0 ml-1.5 inline-flex text-indigo-500 focus:outline-none focus:text-indigo-700" aria-label="Remove small badge">
+                                                    <svg className="h-2 w-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
+                                                        <path stroke-linecap="round" stroke-width="1.5" d="M1 1l6 6m0-6L1 7" />
+                                                    </svg>
+                                                </button>
                                             </span>
                                         </div>
                                     </div>
